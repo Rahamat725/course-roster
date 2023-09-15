5af7ae39b642634611815a78ec1687a7f68bec3d
Sk Rahamatullah <skrahamatullah500@gmail.com>
2023-09-15 17:27:39 +0530

    install tailwind and propType package and setup api data

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1>Course-Registration</h1>
+      
+    </>
+  )
+}
+
+export default App
