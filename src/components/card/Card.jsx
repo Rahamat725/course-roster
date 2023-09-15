86ad6be05670f05e3d1c641a7d621461d180be88
Sk Rahamatullah <skrahamatullah500@gmail.com>
2023-09-15 21:33:18 +0530

    cart structure created

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,4 +1,5 @@
 import { BsBook } from 'react-icons/Bs';
+import { FiDollarSign } from 'react-icons/Fi';
 import PropTypes from 'prop-types';
 const Card = ({card}) => {
     console.log(card)
@@ -11,9 +12,10 @@ const Card = ({card}) => {
            <h2 className='text-base font-semibold mt-4 mb-3'>{title}</h2>
            <p className='text-sm text-[#AEADAD] font-normal leading-6'>{description}</p>
            <div className='flex justify-around items-center mt-4 mb-5'>
-            <p>$ Price: {price}</p>
+            <p className='text-xl'><FiDollarSign>{FiDollarSign}</FiDollarSign></p>
+            <p className='text-base font-medium text-[#737272]'> Price: {price}</p>
              <p><BsBook>{BsBook}</BsBook></p>
-            <p>Credit: {credit_hr}hr</p>
+            <p className='text-[#737272] text-base font-medium'>Credit: {credit_hr}hr</p>
            </div>
          <div className='text-center text-lg font-semibold bg-[#2F80ED] rounded-lg'>
          <button className='text-white py-2'>Select</button>
--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -2,8 +2,24 @@
 
 const Cart = () => {
     return (
-        <div className="w-1/4 border-green-400 border-4">
-            <h2>Cart items</h2>
+        <div className="w-1/4 border-green-400 border-4 ">
+         <div className=" p-6 shadow-lg">
+         <h2 className="text-lg font-bold text-[#2F80ED]">Credit Hour Remaining 7 hr</h2>
+            <hr className="border-2 mt-2"/>
+            <div className="">
+                <h2 className="text-xl font-bold mt-4 mb-5">Course Name</h2>
+                <ol className="list-decimal mb-5">
+                    
+                    <li>Introduction to OOP Python</li>
+                    <li>Database (MySQL)</li>
+                    <li>Introduction to C Programming</li>
+                </ol>
+            </div>
+            <hr className="border-2"/>
+            <h2 className="text-base font-medium mt-4 mb-4">Total Credit Hour : 13</h2>
+            <hr className="border-2"/>
+            <h2 className="text-base font-semibold mt-4">Total Price : 48000 USD</h2>
+         </div>
         </div>
     );
 };
